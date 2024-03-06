37ab4bca0fdf3822a0cfdb0059865b9efd4fc732
tejas mahakal <tejasmahakal740@gmail.com>
2024-03-06 13:18:38 +0530

    1st_react_cra_project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './component/Nav'
+import Main from './component/Main'
+import Card from './component/Card'
+import HeroSection from './component/HeroSection'
+import Footer from './component/Footer'
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Nav/>
+    <HeroSection/>
+     <Main/>
+     <Card/>
+     <Footer/>
+    
+
     </div>
   );
 }
---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+function Header() {
+    return (
+    <div className="header">
+        <img src=""/>
+        <h2>Tejass</h2>
+    </div>
+    );
+    
+}
+
+export default Header ;
